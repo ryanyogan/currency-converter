b4d2f873fe2f0e65f3b8e814210c2c2987ae31d5
ryan <ryanyogan@gmail.com>
2021-08-07 12:34:51 -0500

    added in the logo on home screen

--- App/screens/Home/Home.js
+++ App/screens/Home/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar, Image, Dimensions } from "react-native";
 import { COLORS } from "../../constants/colors";
+
+const screen = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.blue,
     flex: 1,
+    justifyContent: "center",
+  },
+  logoContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    position: "absolute",
+    width: screen.width * 0.25,
+    height: screen.height * 0.25,
+  },
+  logoBackground: {
+    width: screen.width * 0.45,
+    height: screen.height * 0.45,
   },
 });
 
@@ -13,6 +29,18 @@ export const Home = () => {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={COLORS.blue} barStyle="light-content" />
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logoBackground}
+          source={require("../../assets/images/background.png")}
+          resizeMode="contain"
+        />
+        <Image
+          style={styles.logo}
+          source={require("../../assets/images/logo.png")}
+          resizeMode="contain"
+        />
+      </View>
     </View>
   );
 };
