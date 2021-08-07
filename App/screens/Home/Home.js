b77abf3d359d09a9428b5c6efc8262c0681d29d1
ryan <ryanyogan@gmail.com>
2021-08-07 15:09:43 -0500

    update styles and avoid keyboard

--- App/screens/Home/Home.js
+++ App/screens/Home/Home.js
@@ -7,6 +7,7 @@ import {
   Image,
   Dimensions,
   Text,
+  ScrollView,
 } from "react-native";
 import { COLORS } from "../../constants/colors";
 import { Button, ConversionInput } from "../../components";
@@ -15,35 +16,40 @@ const screen = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: COLORS.blue,
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: COLORS.blue,
+  },
+  content: {
+    paddingTop: screen.height * 0.2,
   },
   logoContainer: {
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: 20,
+  },
+  logoBackground: {
+    width: screen.width / 0.45,
+    height: screen.width * 0.45,
   },
   logo: {
     position: "absolute",
     width: screen.width * 0.25,
-    height: screen.height * 0.25,
-  },
-  logoBackground: {
-    width: screen.width * 0.45,
-    height: screen.height * 0.45,
+    height: screen.width * 0.25,
   },
   textHeader: {
     color: COLORS.white,
     fontWeight: "bold",
     fontSize: 30,
-    marginVertical: 20,
     textAlign: "center",
-    shadowColor: "#FFF",
+    marginBottom: 20,
   },
   text: {
+    fontSize: 14,
     color: COLORS.white,
-    fontSize: 13,
     textAlign: "center",
+  },
+  inputContainer: {
+    marginBottom: 10,
   },
 });
 
@@ -55,44 +61,50 @@ export const Home = () => {
 
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={COLORS.blue} barStyle="light-content" />
-      <View style={styles.logoContainer}>
-        <Image
-          style={styles.logoBackground}
-          source={require("../../assets/images/background.png")}
-          resizeMode="contain"
-        />
-        <Image
-          style={styles.logo}
-          source={require("../../assets/images/logo.png")}
-          resizeMode="contain"
-        />
-      </View>
+      <ScrollView>
+        <StatusBar backgroundColor={COLORS.blue} barStyle="light-content" />
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image
+              style={styles.logoBackground}
+              source={require("../../assets/images/background.png")}
+              resizeMode="contain"
+            />
+            <Image
+              style={styles.logo}
+              source={require("../../assets/images/logo.png")}
+              resizeMode="contain"
+            />
+          </View>
 
-      <Text style={styles.textHeader}>Currency Converter</Text>
+          <Text style={styles.textHeader}>Currency Converter</Text>
 
-      <ConversionInput
-        text={baseCurrency}
-        value="123"
-        onButtonPress={() => alert("todo")}
-        onChangeText={(text) => console.log("text", text)}
-        keyboardType="numeric"
-      />
-      <ConversionInput
-        text={quoteCurrency}
-        value="123"
-        editable={false}
-        onButtonPress={() => alert("todo")}
-      />
+          <ConversionInput
+            text={baseCurrency}
+            value="123"
+            onButtonPress={() => alert("todo")}
+            onChangeText={(text) => console.log("text", text)}
+            keyboardType="numeric"
+          />
+          <ConversionInput
+            text={quoteCurrency}
+            value="123"
+            editable={false}
+            onButtonPress={() => alert("todo")}
+          />
 
-      <Text style={styles.text}>
-        {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(
-          date,
-          "MMMM do, yyyy"
-        )}.`}
-      </Text>
+          <Text style={styles.text}>
+            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(
+              date,
+              "MMMM do, yyyy"
+            )}.`}
+          </Text>
 
-      <Button text="Reverse Currencies" onPress={() => alert("todo")} />
+          <Button text="Reverse Currencies" onPress={() => alert("todo")} />
+
+          <View style={{ height: screen.height }} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
