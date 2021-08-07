0c742964c6b9a15857696a6bbb22521b81a67afa
ryan <ryanyogan@gmail.com>
2021-08-07 11:50:48 -0500

    added scroll view

--- App/screens/options.js
+++ App/screens/options.js
@@ -1,37 +1,39 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import { COLORS } from "../constants/colors";
 import { RowItem, RowSeperator } from "../components/RowItem";
 
 export const Options = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <RowItem
-          onPress={() => alert("todo")}
-          text="Themes"
-          rightIcon={
-            <Entypo name="chevron-right" size={20} color={COLORS.blue} />
-          }
-        />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <View>
+          <RowItem
+            onPress={() => alert("todo")}
+            text="Themes"
+            rightIcon={
+              <Entypo name="chevron-right" size={20} color={COLORS.blue} />
+            }
+          />
 
-        <RowSeperator />
+          <RowSeperator />
 
-        <RowItem
-          onPress={() => alert("todo")}
-          text="React Native Basics"
-          rightIcon={<Entypo name="export" size={20} color={COLORS.blue} />}
-        />
+          <RowItem
+            onPress={() => alert("todo")}
+            text="React Native Basics"
+            rightIcon={<Entypo name="export" size={20} color={COLORS.blue} />}
+          />
 
-        <RowSeperator />
+          <RowSeperator />
 
-        <RowItem
-          onPress={() => alert("todo")}
-          text="React Native Tutorials"
-          rightIcon={<Entypo name="export" size={20} color={COLORS.blue} />}
-        />
-      </View>
+          <RowItem
+            onPress={() => alert("todo")}
+            text="React Native Tutorials"
+            rightIcon={<Entypo name="export" size={20} color={COLORS.blue} />}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
